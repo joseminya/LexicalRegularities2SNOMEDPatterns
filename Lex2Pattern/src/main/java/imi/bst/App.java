3f3fc972e79887bb9bca10823a6a26d65de7ec3e
joseminya <jose.minya@gmail.com>
2018-03-13 11:10:34 +0100

    Creation of the Git repository of the project LexicalRegularities2SNOMEDPatterns

---
+++ Lex2Pattern/src/main/java/imi/bst/App.java
@@ -0,0 +1,13 @@
+package imi.bst;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
